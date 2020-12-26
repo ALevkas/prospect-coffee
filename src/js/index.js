e097191cfa1e4064d64a7f3036910516a2a373be
Левченко Александр <alevchenko@softrust.ru>
2020-12-26 15:34:13 +0300

    Skeleton for gulp future project.

---
+++ src/js/index.js
@@ -0,0 +1,106 @@
+let wrapper = document.querySelector('.wrapper');
+
+let pageSlider = new Swiper('.page', {
+    wrapperClass: "page__wrapper",
+    slideClass: "page__screen",
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    parallax: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    watchOverflow: true,
+    speed: 800,
+
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+
+    pagination: {
+        el: ".page__pagination",
+        type: "bullets",
+        clickable: 'true',
+        bulletClass: "page__bullet",
+        bulletActiveClass: "page__bullet_active",
+    },
+
+    scrollbar: {
+        el: ".page__scroll",
+        dragClass: "page__drag-scroll",
+        draggable: true,
+    },
+
+    init: false,
+
+    on: {
+        init: function () {
+            menuSlider();
+            setScrollType();
+            wrapper.classList.add('_loaded');
+        },
+        slideChange: function () {
+            activeClassRemove();
+            menuLinks[pageSlider.realIndex].classList.add('_active');
+        },
+        resize: function() {
+            setScrollType();
+        }
+    }
+})
+
+let menuLinks = document.querySelectorAll('.menu__link');
+
+const menuSlider = () => {
+    if (menuLinks.length > 0) {
+        menuLinks[pageSlider.realIndex].classList.add('_active');
+        for (const [index, item] of menuLinks.entries()) {
+            item.addEventListener('click', (e) => {
+                activeClassRemove();
+
+                pageSlider.slideTo(index, 800);
+                item.classList.add('_active');
+
+                e.preventDefault();
+            });
+        }
+    }
+}
+
+const activeClassRemove = () => {
+    const menuLinkActive = document.querySelector('.menu__link._active');
+    if (menuLinkActive) {
+        menuLinkActive.classList.remove('_active');
+    }
+}
+
+const setScrollType = () => {
+
+    if (wrapper.classList.contains('_free')) {
+        wrapper.classList.remove('_free');
+        pageSlider.params.freeMode = false;
+    }
+
+    for (const slide of pageSlider.slides) {
+        const slideContent = slide.querySelector('.screen__content');
+
+        if (slideContent) {
+            const slideContentHeight = slideContent.offsetHeight;
+
+            if (slideContentHeight > window.innerHeight) {
+                wrapper.classList.add('_free');
+                pageSlider.params.freeMode = true;
+                break;
+            }
+        }
+    }
+}
+
+pageSlider.init();
